aad6aeee7e8812784f7d4078feac5f770c6e6031
RenataTorres <renatatads2018@gmail.com>
2023-03-18 00:09:13 -0300

    feat: create crud app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 const books = [
   {id: 1, "title": "Senhor do Anéis"},
@@ -14,5 +15,32 @@ app.get('/',(req, res) => {
 app.get('/books',(req, res) => {
   res.status(200).json(books);
 });
+
+app.get('/books/:id', (req, res) => {
+  const index = searchBooks(req.params.id);
+  res.json(books[index]);
+});
+
+app.post('/books', (req, res) => {
+  books.push(req.body);
+  res.status(201).send('Livro foi cadastrado com sucesso');
+});
+
+app.put('/books/:id', (req, res) => {
+  const index = searchBooks(req.params.id);
+  books[index].title = req.body.title;
+  res.json(books);
+});
+
+app.put('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const index = searchBooks(id);
+  books.splice(index, 1);
+  res.send(`Livro ${id} apagado com sucesso`);
+});
+
+function searchBooks(id) {
+  return books.findIndex(book => book.id === id);
+}
 
 export default app;
